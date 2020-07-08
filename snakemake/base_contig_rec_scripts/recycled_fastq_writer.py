712c0445123d43dff0ce6ecbf42606a17ed94bdd
Dane <ddeemer@purdue.edu>
2020-07-08 16:50:51 -0400

    Added functionality to rewrite ANI file with updated bins

--- snakemake/base_contig_rec_scripts/recycled_fastq_writer.py
+++ snakemake/base_contig_rec_scripts/recycled_fastq_writer.py
@@ -14,9 +14,10 @@ def get_contig_names_from_recycled(anifile, sample):
         line = i.readline()
         while line:
             if sample == line.split('\t')[0]:
+                contig_ref = line.split('\t')[1]
                 contig = line.split('\t')[2]
                 contig_bin = line.split('\t')[4].strip()
-                contig_names[contig] = contig_bin
+                contig_names[contig] = [contig_bin, contig_ref]
             else:
                 pass
             line = i.readline()
@@ -30,7 +31,7 @@ def write_to_fasta(anifile, sample, assembly):
         while line:
             match = line.strip('>')
             if (line.startswith('>') and match in seqlist):
-                writefile = f"{sample}.{seqlist[match]}.fasta"
+                writefile = f"{sample}.{seqlist[match][0]}.fasta"
                 with open(writefile, 'a') as out:
                     out.write(line + '\n')
                     line = assem.readline()
@@ -41,11 +42,41 @@ def write_to_fasta(anifile, sample, assembly):
                 line = assem.readline().strip()
 
 
+def write_new_ani(anifile, sample, oldani):
+    output = str(oldani.split('.')[0]) + str(".new.txt")
+    new_contigs = get_contig_names_from_recycled(anifile, sample)
+    with open(output, 'w') as o:
+        with open(oldani) as oldani:
+            line = oldani.readline()
+            while line:
+                old_ref = line.split('\t')[1]
+                old_node = line.split('\t')[2]
+                if old_node in new_contigs:
+                    newref = new_contigs[old_node][1]
+                    if newref == old_ref:
+                        writeline = "\t".join(line.split('\t')[0:7])
+                        writeline = writeline + "\t" + new_contigs[old_node][0] + "\t"
+                        writeline = writeline + line.split('\t')[8] + "\n"
+                        o.write(writeline)
+                    else:
+                        pass
+                else:
+                    o.write(line)
+                line = oldani.readline()
+    return "Done"
+
+
 if __name__ == "__main__":
     """ Arguments """
     parser = argparse.ArgumentParser(description="Parser")
     parser.add_argument("-a", "--ANI", help="ANI master file", required=True)
     parser.add_argument("-s", "--Assembly", help="Raw Assembly", required=True)
     parser.add_argument("-q", "--Query", help="Query Sample", required=True)
+    parser.add_argument("-o", "--OldANI", help="Original ANI file",
+                        required=False)
     argument = parser.parse_args()
     write_to_fasta(argument.ANI, argument.Query, argument.Assembly)
+    if argument.OldANI:
+        write_new_ani(argument.ANI, argument.Query, argument.OldANI)
+    else:
+        pass
