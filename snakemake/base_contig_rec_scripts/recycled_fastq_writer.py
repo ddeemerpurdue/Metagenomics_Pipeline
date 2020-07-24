0bae6675390b946bffc0bfd3546119b9ba4d731e
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-07-24 07:08:18 -0400

    Aesthetics
    Updated some comments and few minor aesthetic changes

--- snakemake/base_contig_rec_scripts/recycled_fastq_writer.py
+++ snakemake/base_contig_rec_scripts/recycled_fastq_writer.py
@@ -2,21 +2,35 @@
 Program designed to take in as input a raw assembly fasta file,
 a standardized ANI file, and the sample name (column 0 sample in
 ANI file). Output are {sample}.{bin}.fasta files.
+Note: This file should also be found in the Contig_Purifier repository
+
+Example usage:
+$ python recycledBinFastaWriter.py -r repatriatedFile.txt -s assemblyFile.fasta -q sampleName
 '''
 
 import argparse
 
 
-def get_contig_names_from_recycled(anifile, sample):
+def get_contig_names_from_recycled(repat, sample):
+    '''
+    Function to read in a repatriated output and write
+    a dictionary in the form:
+    {node_identifier:[bin_id, reference_sample]}
+    '''
     contig_names = {}
-    with open(anifile) as i:
+    with open(repat) as i:
         line = i.readline()  # Skip header
         line = i.readline()
         while line:
-            if sample == line.split('\t')[0]:
-                contig_ref = line.split('\t')[1]
-                contig = line.split('\t')[2]
-                contig_bin = line.split('\t')[4].strip()
+            line = line.split('\t')
+            if sample == line[0]:
+                contig_ref = line[1]
+                contig = line[2]
+                if contig.startswith('>'):
+                    pass
+                else:
+                    contig = '>' + contig
+                contig_bin = line[4].strip()
                 contig_names[contig] = [contig_bin, contig_ref]
             else:
                 pass
@@ -24,14 +38,18 @@ def get_contig_names_from_recycled(anifile, sample):
     return contig_names
 
 
-def write_to_fasta(anifile, sample, assembly):
-    seqlist = get_contig_names_from_recycled(anifile, sample)
+def write_to_fasta(repat, sample, assembly):
+    '''
+    Function that searches an assembly based on a dictionary
+    with node identifiers as keys and bin/reference as values
+    and writes a new fasta file
+    '''
+    seqdic = get_contig_names_from_recycled(repat, sample)
     with open(assembly) as assem:
         line = assem.readline().strip()
         while line:
-            match = line.strip('>')
-            if (line.startswith('>') and match in seqlist):
-                writefile = f"{sample}.{seqlist[match][0]}.fasta"
+            if (line.startswith('>') and line in seqdic):
+                writefile = f"{sample}.{seqdic[line][0]}.fasta"
                 with open(writefile, 'a') as out:
                     out.write(line + '\n')
                     line = assem.readline()
@@ -42,9 +60,9 @@ def write_to_fasta(anifile, sample, assembly):
                 line = assem.readline().strip()
 
 
-def write_new_ani(anifile, sample, oldani):
-    output = str(oldani.split('.')[0]) + str(".new.txt")
-    new_contigs = get_contig_names_from_recycled(anifile, sample)
+def write_new_ani(repat, sample, oldani):
+    output = str(oldani.rsplit('.', 1)[0]) + str(".new.txt")
+    new_contigs = get_contig_names_from_recycled(repat, sample)
     with open(output, 'w') as o:
         with open(oldani) as oldani:
             line = oldani.readline()
@@ -69,14 +87,15 @@ def write_new_ani(anifile, sample, oldani):
 if __name__ == "__main__":
     """ Arguments """
     parser = argparse.ArgumentParser(description="Parser")
-    parser.add_argument("-a", "--ANI", help="ANI master file", required=True)
+    parser.add_argument("-r", "--Repat", help="Repatriated file",
+                        required=True)
     parser.add_argument("-s", "--Assembly", help="Raw Assembly", required=True)
     parser.add_argument("-q", "--Query", help="Query Sample", required=True)
     parser.add_argument("-o", "--OldANI", help="Original ANI file",
                         required=False)
     argument = parser.parse_args()
-    write_to_fasta(argument.ANI, argument.Query, argument.Assembly)
+    write_to_fasta(argument.Repat, argument.Query, argument.Assembly)
     if argument.OldANI:
-        write_new_ani(argument.ANI, argument.Query, argument.OldANI)
+        write_new_ani(argument.Repat, argument.Query, argument.OldANI)
     else:
         pass
