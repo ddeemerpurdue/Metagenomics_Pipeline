ba98420ffcef5d93be49e12642793d243b81c880
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-08-27 13:52:20 -0400

    Update allEnvironments.py

--- snakemake/envs/allEnvironments.py
+++ snakemake/envs/allEnvironments.py
@@ -3,10 +3,11 @@
 channels:
   - bioconda
 dependencies:
+  - bbmap=38.84=h516909a_0
+  - bowtie2=2.4.1=py37h8270d21_3
   - diamond=0.9.35=h56fc30b_0
   - freebays=2.10.2=h5ab3b9f_0
-  - bowtie2=2.4.1=py37h8270d21_3
-  - samtools=1.6=h02bfda8_2
-  - bbmap=38.84=h516909a_0
   - maxbin2=2.2.7=he1b5a44_0
+  - mummer=3.23=pl526he1b5a44_10
   - refinem=0.1.1=py_0
+  - samtools=1.6=h02bfda8_2
