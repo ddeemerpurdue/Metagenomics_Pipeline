6f6f0f3a1d0638346d2899515679b43250f1a831
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-08-27 11:53:00 -0400

    Create allEnvironments.py

---
+++ snakemake/envs/allEnvironments.py
@@ -0,0 +1,10 @@
+# Running list of all environments currently being used:
+
+channels:
+  - bioconda
+dependencies:
+  - bowtie2=2.4.1=py37h8270d21_3
+  - samtools=1.6=h02bfda8_2
+  - bbmap=38.84=h516909a_0
+  - maxbin2=2.2.7=he1b5a44_0
+  - refinem=0.1.1=py_0
