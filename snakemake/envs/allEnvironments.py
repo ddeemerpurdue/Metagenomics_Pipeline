7f8818c8a78b895bbee3821c9c1a1e669d3c23b0
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-08-27 12:13:11 -0400

    Update allEnvironments.py

--- snakemake/envs/allEnvironments.py
+++ snakemake/envs/allEnvironments.py
@@ -3,6 +3,8 @@
 channels:
   - bioconda
 dependencies:
+  - diamond=0.9.35=h56fc30b_0
+  - freebays=2.10.2=h5ab3b9f_0
   - bowtie2=2.4.1=py37h8270d21_3
   - samtools=1.6=h02bfda8_2
   - bbmap=38.84=h516909a_0
