b0e141e4e964abeb7f0c9c7f28c35c0e627d39b5
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-08-31 10:32:52 -0400

    Update make_abund_list.py

--- snakemake/baseMetagenomicsScripts/make_abund_list.py
+++ snakemake/baseMetagenomicsScripts/make_abund_list.py
@@ -8,29 +8,23 @@ $ python make_abund_list.py -i <path/to/input/dir/> -o <outputfile.txt>
 """
 import os
 
-# import argparse
-
-''' Arguments '''
-# parser = argparse.ArgumentParser(description="Parser")
-# parser.add_argument("-i", "--Input",
-#                     help="Directory containing .abundance.txt",
-#                     required=True)
-# parser.add_argument("-o", "--Output",
-#                     help="Output file to write to",
-#                     required=True)
-# argument = parser.parse_args()
-''' Arguments '''
 infile = str(snakemake.input)
 outfile = str(snakemake.output)
 
-wdir = os.getcwd()
 
-mylist = []
-infile = infile.split()
-for f in infile:
-    if f.endswith('abundance.txt'):
-        mylist.append(f)
+def write_abundance_list(snakemake_in, snakemake_out):
+    mylist = []
+    infile = snakemake_in.split()
+    for f in infile:
+        if str(os.path.isfile(f)):
+            mylist.append(f)
+        else:
+            return f"File: {f} does not exist!!!"
+    with open(outfile, 'w') as o:
+        for f in mylist:
+            o.write(str(f) + '\n')
+    return 0
 
-with open(outfile, 'w') as o:
-    for f in mylist:
-        o.write(str(f) + '\n')
+
+if __name__ == "__main__":
+    write_abundance_list(infile, outfile)
