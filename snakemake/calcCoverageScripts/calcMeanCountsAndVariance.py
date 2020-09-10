e0524a5b7021a431246483660a23a33a7a3ec3aa
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-09-10 16:02:57 -0400

    Update calcMeanCountsAndVariance.py

--- snakemake/calcCoverageScripts/calcMeanCountsAndVariance.py
+++ snakemake/calcCoverageScripts/calcMeanCountsAndVariance.py
@@ -4,7 +4,7 @@ Script to calculate the variance between forward and reverse
 reads given htseq-count output for each file.
 If the -o/--Output flag is given, the mean of F/R will be
 computed and a new count file will be created with the mean
-count.
+count and variance.
 Example usage:
 $ python calcFRVariance.py -f <forward.txt> -r <reverse.txt> \
 -o <optionally-write-output.txt>
@@ -13,23 +13,25 @@ $ python calcFRVariance.py -f <forward.txt> -r <reverse.txt> \
 
 def calculate_FR_deviation(forward, reverse, output=False):
     '''
-    Fx that reads in two files, calculated deviation and,
-    if specified, writes a new mean output file.
+    Fx that reads in two files, calculates deviation and,
+    if specified, writes a new file that contains each feature,
+    the variance between F and R, and the mean value.
     '''
+    sample=str(forward).rsplit('/', 1)[1].split('_R1')[0]
     n = 0
     mean_values = {}
     mean_variances = []
     if output:
         with open(output, 'a') as o:
-            o.write(f"Feature\tMeanCPM\n")
+            o.write(f"Feature\tMeanCPM\tVariance\tSample\n")
     with open(forward) as f:
         with open(reverse) as r:
             f_line, r_line = f.readline(), r.readline()
             f_line, r_line = f.readline().strip(), r.readline().strip()
             while (f_line and r_line):
                 n += 1
-                f_line = f.readline().split('\t')
-                r_line = r.readline().split('\t')
+                f_line = f_line.split('\t')
+                r_line = r_line.split('\t')
                 f_feat, r_feat = f_line[0], r_line[0]
                 try:
                     f_val, r_val = float(f_line[1]), float(r_line[1])
@@ -44,12 +46,12 @@ def calculate_FR_deviation(forward, reverse, output=False):
                     mean_variances.append(var)
                     if output:
                         with open(output, 'a') as o:
-                            o.write(f"{f_feat}\t{mean}\n")
+                            o.write(f"{f_feat}\t{mean}\t{var}\t{sample}\n")
                 f_line, r_line = f.readline().strip(), r.readline().strip()
     mean_variance = (sum(mean_variances) / len(mean_variances))
     if output:
         with open(output, 'a') as o:
-            o.write(f"Mean_Variance\t{mean_variance}")
+            o.write(f"MeanVariance\t0\t{mean_variance}\tNA\n")
     print(f"The mean variance is: {mean_variance}")
     return 0
 
@@ -62,13 +64,3 @@ if __name__ == "__main__":
                         help="Forward file")
     parser.add_argument("-r", "--Reverse", required=True,
                         help="Reverse file")
-    parser.add_argument("-o", "--Output", required=False,
-                        help="Output file name")
-    argument = parser.parse_args()
-    ''' Arguments '''
-    if argument.Output:
-        calculate_FR_deviation(argument.Forward, argument.Reverse,
-                               argument.Output)
-    else:
-        calculate_FR_deviation(argument.Forward, argument.Reverse)
-
