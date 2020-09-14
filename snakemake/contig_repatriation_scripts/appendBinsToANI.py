83bbde42f444c46583d6a59fa2ad70f1135a80af
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-09-14 08:12:51 -0400

    Added annotation and fixed naming issues.

--- snakemake/contig_repatriation_scripts/appendBinsToANI.py
+++ snakemake/contig_repatriation_scripts/appendBinsToANI.py
@@ -1,15 +1,20 @@
 '''
 Author: Dane
 Program designed to take in the raw input from the fastANI snakemake
-pipeline and standardize it with bin information from RefineM output
-(or whatever the most up-to-date output is from).
+pipeline and standardize it with bin information.
 Input requires a tab-delimited list of the binfiles as well.
+Note: Each bin to node identification file (1 per sample) NEEDS to be
+labeled in the format: <sample>.*
+where <sample> matches the sample completeley in the ANI file.
+For example, if in the ani file that sample names are 'particle' and
+'supernatant', the bin identification files must have a prefix starting
+with 'particle.' and 'supernatant.', such as 'particle.originalBins.txt'
+and 'supernatant.originalBins.txt'.
 Example useage:
 $ python add_bins.py -a ani.txt -o out.txt -b bin1.txt bin2.txt
-
-Note: bin files need to be in the specified format: sample.Bins.txt
+Again: bin files need to be in the specified format: sample.*
 '''
-
+import os
 import argparse
 
 parser = argparse.ArgumentParser(description="Parser")
@@ -23,19 +28,16 @@ parser.add_argument("-o", "--Output", help="Standardized ANI file to write to",
 argument = parser.parse_args()
 
 
-binlist = []
-for b in argument.Bins:
-    binlist.append(b)
-print(binlist)
-
-
 def get_bindict(binfile):
-    # Start with query bins
+    '''
+    Open a bin identification file and output a dictionary in
+    the form: dict[node] = bin_number
+    '''
     bindict = {}
     with open(binfile) as qf:
         line = qf.readline()
         while line:
-            bin_name = line.split('\t')[0].split('.')[1].split('_')[0]
+            bin_name = line.split('\t')[0]
             node = line.split('\t')[1].strip()
             node = node.strip('>')
             bindict[node] = bin_name
@@ -44,11 +46,17 @@ def get_bindict(binfile):
 
 
 def get_all_bindict(binlist):
+    '''
+    Given a list of bin identification files, read each in a dictionary
+    in the form of: dict[name][node] = bin_number
+    This is a nested dictionary 1 level higher than get_bindict.
+    '''
     all_dict = {}
     for file in binlist:
+        name = os.path.basename(file)
+        name = name.split('.')[0]
         dic = get_bindict(file)
-        key = str(file).split('.')[0]
-        all_dict[key] = dic
+        all_dict[name] = dic
     return all_dict
 
 
@@ -61,8 +69,10 @@ def append_bins_to_ani(binfile, anifile, output):
             constant_line = af.readline()
             while constant_line:
                 line = constant_line.split('\t')
+                # Below, grab sample name out of first 2 fields in raw ANI output
                 quer = line[0].split('/')[1].split('_')[0]
                 refer = line[1].split('/')[1].split('_')[0]
+                # Do not need ANI results within samples (e.g., R1 vs. R1)
                 if quer != refer:
                     quer_node = line[0].split('/')[2].rsplit('.', 1)[0]
                     reference_node = line[1].split('/')[2].rsplit('.', 1)[0]
@@ -90,4 +100,13 @@ def append_bins_to_ani(binfile, anifile, output):
 
 
 if __name__ == "__main__":
-    append_bins_to_ani(binlist, argument.ANI, argument.Output)
+    import argparse
+    parser = argparse.ArgumentParser(description="Parser")
+    parser.add_argument("-b", "--Bins", required=True, nargs="*",
+                        help="One or more files with contig name and bin info")
+    parser.add_argument("-a", "--ANI", help="ANI file created from FASTANI",
+                        required=True)
+    parser.add_argument("-o", "--Output", help="Standardized ANI file to write to",
+                        required=True)
+    argument = parser.parse_args()
+    append_bins_to_ani(argument.Bins, argument.ANI, argument.Output)
