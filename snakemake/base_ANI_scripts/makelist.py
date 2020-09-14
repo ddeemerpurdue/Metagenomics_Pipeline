419720ada1c4e27013c26459ec077d5a092f26cf
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-09-14 07:13:29 -0400

    Update makelist.py

--- snakemake/base_ANI_scripts/makelist.py
+++ snakemake/base_ANI_scripts/makelist.py
@@ -1,5 +1,7 @@
-# Step 1:
-
+'''
+Author: Dane
+Module that is snakemake-specific and takes in 
+'''
 import os
 import sys
 
@@ -12,20 +14,23 @@ out_list = out_list.split()
 
 print(f"{in_contigs}\n{out_list}")
 
-
-for directory, out in zip(in_contigs, out_list):
-    d = directory.rsplit('/', 1)[0]
+# in_contigs contains a list of all the *.complete.tkn files created from
+# split_filtered_contigs rule, while out_list contains output files where
+# each line is the path to a contig
+for in_contig, out in zip(in_contigs, out_list):
+    # Remove basename to get location of .fa files
+    directory = in_contig.rsplit('/', 1)[0]
     line = []
-    print(d)
-    print(out)
-    for file in os.listdir(d):
+    # Go through every .fa file in the directory
+    for file in os.listdir(directory):
         if file.endswith('.fa'):
             line.append(file)
         else:
             pass
     line = sorted(line)
+    # Write every .fa file from the directory to the output file (lists/{sample}.{length}.txt
     with open(out, 'w') as o:
         for l in line:
-            o.write(str(d) + '/' + str(l))
+            o.write(str(directory) + '/' + str(l))
             o.write('\n')
 
