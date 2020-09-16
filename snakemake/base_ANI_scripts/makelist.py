695b8c975b5cfdf9e48e16f698dc848f1388f2fa
Dane <64015522+ddeemerpurdue@users.noreply.github.com>
2020-09-16 13:08:52 -0400

    Update makelist.py

--- snakemake/base_ANI_scripts/makelist.py
+++ snakemake/base_ANI_scripts/makelist.py
@@ -1,35 +1,30 @@
 '''
 Author: Dane
-Module that is snakemake-specific and takes in 
+Module that is snakemake-specific and takes in a directory of .fa files
+and writes to one list.
 '''
 import os
 import sys
 
 # Initialize input parameters from snakemake into a list
-in_contigs = str(snakemake.input)
+in_tokens = str(snakemake.input)
 out_list = str(snakemake.output)
 # Put the input inside of a list
-in_contigs = in_contigs.split()
+in_tokens = in_tokens.split()
 out_list = out_list.split()
 
-print(f"{in_contigs}\n{out_list}")
+print(f"{in_tokens}\n{out_list}")
 
-# in_contigs contains a list of all the *.complete.tkn files created from
+# in_tokens contains a list of all the *.complete.tkn files created from
 # split_filtered_contigs rule, while out_list contains output files where
 # each line is the path to a contig
-for in_contig, out in zip(in_contigs, out_list):
+for token, out in zip(in_tokens, out_list):
     # Remove basename to get location of .fa files
-    directory = in_contig.rsplit('/', 1)[0]
-    line = []
-    # Go through every .fa file in the directory
-    for file in os.listdir(directory):
-        if file.endswith('.fa'):
-            line.append(file)
-        else:
-            pass
-    line = sorted(line)
-    # Write every .fa file from the directory to the output file (lists/{sample}.{length}.txt
+    directory = token.rsplit('/', 1)[0]
+    # Go through every .fa file in the directory and write to out
     with open(out, 'w') as o:
-        for l in line:
-            o.write(str(directory) + '/' + str(l))
-            o.write('\n')
+        for file in os.listdir(directory):
+            if file.endswith('.fa'):
+                o.write(str(directory) + '/' + str(file) + '\n')
+            else:
+                pass
